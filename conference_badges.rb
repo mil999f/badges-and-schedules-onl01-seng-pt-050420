8358e2a7a3e3009ef1a74ae2317859530bff2a80
Milly Flores <miflo0h@gmail.com>
2020-05-15 23:21:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,13 +3,12 @@ def badge_maker(name)
 end 
   
 def batch_badge_creator(attendees)
-  attendees.collect{|name| puts "Hello, my name is #{name}."
+  attendees.collect{|name| puts "Hello, my name is #{name}."}
 end
    
 def assign_rooms(attendees)
   attendees.each_with_index do |attendee, room|
     puts "Hello, #{attendee}! You'll be assigned to  #{room + 1}"
-    end
   end
     
 def printer(a)
