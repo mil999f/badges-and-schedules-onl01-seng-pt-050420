bfe7f4a3e9a4b1a92aea8ccc55f188816fb526d1
Milly Flores <miflo0h@gmail.com>
2020-05-15 23:26:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end 
   
 def batch_badge_creator(attendees)
-  return attendees.collect{|name| puts "Hello, my name is #{name}."}
+  return attendees.collect{|name| p "Hello, my name is #{name}."}
 end
    
 def assign_rooms(attendees)
